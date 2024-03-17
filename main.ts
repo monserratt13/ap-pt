6aee2c91af5ee8dc0ab7bde7c6effb719ca59185
monserratt13 <156123495+monserratt13@users.noreply.github.com>
2024-03-16 23:10:09 -0500

    Worked on function and scoring system seems to work now. Added an enemy and a loop.

--- main.ts
+++ main.ts
@@ -1,25 +1,35 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    if (info.score() == 0) {
-        game.gameOver(false)
-        game.reset()
-    }
+    game.gameOver(false)
+    game.reset()
 })
-function tilemapChange (mySprite: Sprite) {
+function tilemapChange (list: tiles.TileMapData[]) {
     if (info.score() == 1) {
-        list = [tilemap`level2`, tilemap`level4`]
+        info.setScore(1)
+        tiles.setCurrentTilemap(list._pickRandom())
+    }
+    if (info.score() == 2) {
+        info.setScore(2)
         tiles.setCurrentTilemap(list._pickRandom())
     }
     if (info.score() == 3) {
+        info.setScore(3)
         game.gameOver(true)
         game.reset()
     }
+    tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     info.changeScoreBy(1)
-    tilemapChange(mySprite)
+    tilemapChange(list)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.gameOver(false)
+    game.reset()
 })
 let list: tiles.TileMapData[] = []
 let mySprite: Sprite = null
+info.startCountdown(60)
+info.setScore(0)
 tiles.setCurrentTilemap(tilemap`level1`)
 mySprite = sprites.create(img`
     . . . . . f f f f . . . . . 
@@ -39,8 +49,154 @@ mySprite = sprites.create(img`
     . . f f 3 b 3 b 3 3 f f . . 
     . . . . f f b b f f . . . . 
     `, SpriteKind.Player)
+let badguy = sprites.create(img`
+    . . . . . c c c c c c c . . . . 
+    . . . . c 6 7 7 7 7 7 6 c . . . 
+    . . . c 7 c 6 6 6 6 c 7 6 c . . 
+    . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+    . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+    . . f 7 8 1 f f 1 6 7 7 7 f . . 
+    . . f 6 f 1 f f 1 f 7 7 7 f . . 
+    . . . f f 2 2 2 2 f 7 7 6 f . . 
+    . . c c f 2 2 2 2 7 7 6 f c . . 
+    . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+    c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+    f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+    f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+    . . f f c c c c c c c c . . . . 
+    `, SpriteKind.Enemy)
 scene.cameraFollowSprite(mySprite)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(18, 23))
+tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
 controller.moveSprite(mySprite, 100, 100)
-info.startCountdown(50)
-info.setScore(0)
+list = [tilemap`level2`, tilemap`level4`, tilemap`level11`]
+for (let index = 0; index < 2; index++) {
+    badguy = sprites.create(img`
+        . . . . . c c c c c c c . . . . 
+        . . . . c 6 7 7 7 7 7 6 c . . . 
+        . . . c 7 c 6 6 6 6 c 7 6 c . . 
+        . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+        . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+        . . f 7 8 1 f f 1 6 7 7 7 f . . 
+        . . f 6 f 1 f f 1 f 7 7 7 f . . 
+        . . . f f 2 2 2 2 f 7 7 6 f . . 
+        . . c c f 2 2 2 2 7 7 6 f c . . 
+        . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+        c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+        f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+        f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+        . . f f c c c c c c c c . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(badguy, sprites.castle.tileGrass2)
+    animation.runImageAnimation(
+    badguy,
+    [img`
+        . . . . . c c c c c c c . . . . 
+        . . . . c 6 7 7 7 7 7 6 c . . . 
+        . . . c 7 c 6 6 6 6 c 7 6 c . . 
+        . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+        . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+        . . f 7 8 1 f f 1 6 7 7 7 f . . 
+        . . f 6 f 1 f f 1 f 7 7 7 f . . 
+        . . . f f 2 2 2 2 f 7 7 6 f . . 
+        . . c c f 2 2 2 2 7 7 6 f c . . 
+        . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+        c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+        f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+        f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+        . . f f c c c c c c c c . . . . 
+        `,img`
+        . . . . . . c c c c c c c . . . 
+        . . . . . c f f 6 6 f f 7 c . . 
+        . . . . c 7 6 6 6 6 6 6 7 6 c . 
+        . . . c 7 7 7 7 7 7 7 7 7 7 c . 
+        . . . c 7 8 1 f f 1 6 7 7 7 c . 
+        . . . f 6 f 1 f f 1 f 7 7 7 f . 
+        . . . f 6 f 2 2 2 2 f 7 7 7 f . 
+        . . c c 6 f 2 2 2 2 f 7 7 6 f . 
+        . c 7 7 7 7 2 2 2 2 7 7 f c . . 
+        c 7 1 1 1 7 7 7 7 7 c c 7 7 c . 
+        f 1 1 1 1 1 7 7 7 f c 6 7 7 7 c 
+        f 1 1 1 1 1 1 6 f c c 6 6 6 c c 
+        f 6 1 1 1 1 1 6 6 c 6 6 6 c . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . f 6 1 1 1 1 6 6 6 6 6 c . . . 
+        . . f f c c c c c c c c . . . . 
+        `,img`
+        . . . . . . c c c c c c c . . . 
+        . . . . . c f f 6 6 f f 7 c . . 
+        . . . . c 7 6 6 6 6 6 6 7 6 c . 
+        . . . c 7 7 7 7 7 7 7 7 7 7 c . 
+        . . . c 7 8 1 f f 1 6 7 7 7 c . 
+        . . . f 6 f 1 f f 1 f 7 7 7 f . 
+        . . . f 6 f 2 2 2 2 f 7 7 7 f . 
+        . . c c 6 f 2 2 2 2 f 7 7 6 f . 
+        . c 7 7 7 7 2 2 2 2 7 7 f c . . 
+        c 7 1 1 1 7 7 7 7 7 c c 7 7 c . 
+        f 1 1 1 1 1 7 7 7 f c 6 7 7 7 c 
+        f 1 1 1 1 1 1 6 f c c 6 6 6 c c 
+        f 6 1 1 1 1 1 6 6 c 6 6 6 c . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . f 6 1 1 1 1 6 6 6 6 6 c . . . 
+        . . f f c c c c c c c c . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . c c c c c 
+        . . . . . . . . . c c 7 7 7 6 c 
+        . . . . . . . . c c 7 7 7 c c . 
+        . . . . . . . . c 6 7 7 c . . . 
+        . . . . . . . . c 6 6 6 c . . . 
+        . . . c c c c c c 6 6 6 c c . . 
+        . . c 6 7 7 7 7 6 c c 6 6 6 c . 
+        . c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+        c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+        c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+        f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+        f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+        . c 1 c f f 1 c 7 6 f 6 6 c c . 
+        . c c c c c c c c c c c c . . . 
+        `,img`
+        . . . . . . . . . . . c c c c c 
+        . . . . . . . . . c c 7 7 7 6 c 
+        . . . . . . . . c c 7 7 7 c c . 
+        . . . . . . . . c 6 7 7 c . . . 
+        . . . . . . . . c 6 6 6 c . . . 
+        . . . . . . . . c 6 6 6 c c . . 
+        . . . c c c c c c c 6 6 6 c c . 
+        . . c 6 7 7 7 7 6 c c 6 6 6 c . 
+        . c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+        c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+        c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+        f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+        f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+        . f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
+        . c 1 c f f 1 c 7 6 f 6 6 c c . 
+        . c c c c c c c c c c c c . . . 
+        `,img`
+        . . . . . . . . . . . c c c c c 
+        . . . . . . . . . c c 7 7 7 6 c 
+        . . . . . . . . c c 7 7 7 c c . 
+        . . . . . . . . c 6 7 7 c . . . 
+        . . . . . . . . c 6 6 6 c . . . 
+        . . . . . . . . c 6 6 6 c c . . 
+        . . . c c c c c c c 6 6 6 c c . 
+        . . c 6 7 7 7 7 6 c c 6 6 6 c . 
+        . c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+        c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+        c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+        f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+        f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+        . f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
+        . c 1 c f f 1 c 7 6 f 6 6 c c . 
+        . c c c c c c c c c c c c . . . 
+        `],
+    200,
+    true
+    )
+}
